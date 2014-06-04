84790acbf3a639b0d345415eef15a41d0f99e816
phennig <paul.hennig@d214.org>
2014-06-04 08:09:20 -0500

    TableView/SearchBar

--- Teacher/ViewController.m
+++ Teacher/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+
+@property NSMutableArray *students;
 
 @end
 
@@ -17,13 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end

17a005410d69edfb636a18634615f5e55608c21c
bdbrown214 <bdbrown214@gmail.com>
2014-06-04 10:46:13 -0500

    Bob’s Initial Commit

---
+++ Teacher/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  Teacher
+//
+//  Created by Robert D. Brown on 6/4/14.
+//  Copyright (c) 2014 PHMobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ Teacher/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  Teacher
+//
+//  Created by Robert D. Brown on 6/4/14.
+//  Copyright (c) 2014 PHMobileMakers. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end

b1661df1414e2ca9cd2667cd28a99cbc8128491d
Kristen Fisher <kristen.fisher@d214.org>
2014-06-04 11:13:04 -0500

    kristen’s initial commit

---
+++ Teacher/AddStudentViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddStudentViewController.h
+//  Teacher
+//
+//  Created by Kristen L. Fisher on 6/4/14.
+//  Copyright (c) 2014 PHMobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddStudentViewController : UIViewController
+
+@end
---
+++ Teacher/AddStudentViewController.m
@@ -0,0 +1,30 @@
+//
+//  AddStudentViewController.m
+//  Teacher
+//
+//  Created by Kristen L. Fisher on 6/4/14.
+//  Copyright (c) 2014 PHMobileMakers. All rights reserved.
+//
+
+#import "AddStudentViewController.h"
+
+@interface AddStudentViewController ()
+//@property (weak, nonatomic)
+//@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+
+
+
+@end
+
+@implementation AddStudentViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
